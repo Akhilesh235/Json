73557feae8e3695fb750914a38002e4a296f7d84
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-15 16:22:20 +0800

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W6D5_Training_Ses1
+{
+    public class Employee
+    {
+        public string Name { get;  set; }
+        public Guid Id { get;  set; }
+
+        public string Designation { get;  set; }
+
+        public string Address { get;  set; }
+
+        public double Salary { get;  set; }
+
+        public Employee()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace W6D5_Training_Ses1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Akhi",
+                Address = "23 Pears Avenue",
+                Designation = "CEO",
+                Salary = 10000000,
+            };
+            string  employeeJson = JsonConvert.SerializeObject(emp);
+            //File.Create("Employee.Json");
+            File.WriteAllText("Employee.json", employeeJson);
+           
+            //Console.ReadLine();
+            Employee empTemp =JsonConvert.DeserializeObject<Employee>(File.ReadAllText("Employee.json"));
+            string employeeJson1 = JsonConvert.SerializeObject(empTemp);
+            File.WriteAllText("Employee1.json", employeeJson1);
+            Console.ReadLine();
+        }
+
+    }
+}
